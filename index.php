c5c2b5fd6904ab0de4dc8e612953e31cd115c11d
Houssam OUATMANI <ouatmanihoussam@gmail.com>
2024-10-23 11:41:35 +0200

    un autre commit

--- index.php
+++ index.php
@@ -24,6 +24,13 @@ echo "changement depuis la branch master";
     git diff
     git restore <filename>
     git reset
+    git branch  <nom de la branche> Creation
+    git branch  lister les branches
+    git branch -d  <nom de la branche> Suppression
+    git checkout <nom de la branche> navigation d'un branche à une autre
+    git switch <nom de la branche> navigation d'un branche à une autre
+    git merge <nom de la branche> fusioner la branche passé en parametre dans la branche de destination
+  
 */
 
 
