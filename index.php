b613355dd07dae9a90cc07ff50c06629cafc2f3e
Houssam OUATMANI <ouatmanihoussam@gmail.com>
2024-10-23 11:12:48 +0200

    second commit on master branch

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
 <?php 
 
 echo "Fichier modifié";
+
+echo "un autre echo";
+
+echo "un changement";
+
+
+// *** Créer un dépôt Git sur ton ordinateur et y ajouter des fichiers.
+// *** Ajoute 5 commits
+
+// *** Commandes vues
+
+/*
+    git init
+    git add <filename>
+    git status
+    git commit -m "<message>"
+    git log
+    git diff
+    git restore <filename>
+    git reset
+*/
